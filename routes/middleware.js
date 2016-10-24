1e18bd32fcecfb17f44a2635d53e156b0c85f6e7
Kyle Warren <qarren@gmail.com>
2016-10-24 08:38:41 -0700

    max count is 40

--- routes/middleware.js
+++ routes/middleware.js
@@ -62,6 +62,8 @@ exports.initLocals = function (req, res, next) {
     locals.songs = req.user ? songs : publicSongs;
     locals.latestSong = _.last(songs);
     locals.aboutText = (aboutPage && aboutPage.description) || '30 days 30 songs is a playlist of songs written and recorded by musicians for a Trump-free America.';
+    locals.maxCount = 40;
+    /*
     locals.maxCount = (count => {
       for (let max = 30; max < 70; max += 10) {
         if (count < max + 1) {
@@ -71,6 +73,7 @@ exports.initLocals = function (req, res, next) {
 
       return 70;
     })(publicSongs.length);
+    */
     locals.logoSuffix = locals.maxCount === 30 ? '' : `-${locals.maxCount}`;
 
     next();
