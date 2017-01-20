a0378fff5f484722d1156778e20059e3daf658a7
Kyle Warren <qarren@gmail.com>
2017-01-20 07:28:35 -0800

    increase max count to number of songs

--- routes/middleware.js
+++ routes/middleware.js
@@ -62,7 +62,7 @@ exports.initLocals = function (req, res, next) {
     locals.songs = req.user ? songs : publicSongs;
     locals.latestSong = _.last(songs);
     locals.aboutText = (aboutPage && aboutPage.description) || '30 days 30 songs is a playlist of songs written and recorded by musicians for a Trump-free America.';
-    locals.maxCount = 50;
+    locals.maxCount = locals.songs.length;
     /*
     locals.maxCount = (count => {
       for (let max = 30; max < 70; max += 10) {
@@ -74,7 +74,7 @@ exports.initLocals = function (req, res, next) {
       return 70;
     })(publicSongs.length);
     */
-    locals.logoSuffix = locals.maxCount === 30 ? '' : `-${locals.maxCount}`;
+    locals.logoSuffix = `-50`;
 
     next();
 
